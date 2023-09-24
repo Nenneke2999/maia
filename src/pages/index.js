2884c869ba93753e5a0acd38dbde56e6994bada8
Nenneke2999 <arinamironova2911@gmail.com>
2023-09-24 04:51:12 +0300

    Front

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,134 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Messages from '@/components/Messages'
+import { useState, useRef, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import styles from '@/styles/Home.module.css'
+import Input from '@/components/Input';
+import Parametrs from '@/components/Parametrs';
 
-const inter = Inter({ subsets: ['latin'] })
+// Генерируем случайный цвет аватара
+function randomColor() {
+  return '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16);
+}
 
 export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+  // начальное состояние чата
+  const [messages, setMessages] = useState([]);
+  const [members, setMembers] = useState([]);
+  const [me, setMe] = useState({
+    username: 'Вы',
+    color: randomColor(),
+  });
+
+  const ipAddress = useRef(''); // Ip пользователя
+  const IDfromURL = useRef(''); // session_id из ссылки
+
+  const user_dictionary = {}
+
+  const addUser = (ip_address, user_id) => {
+    if (!user_dictionary[ip_address]) {
+        user_dictionary[ip_address] = user_id
+    }
+  }
+
+  const router = useRouter();
+
+  useEffect(() => {
+    fetch('https://api.ipify.org?format=json') // В любом случае достаем IP пользователя
+        .then(response => response.json())
+        .then(data => localStorage.setItem("ip_address", data.ip))
+        .catch(error => console.log(error))
+    ipAddress.current = localStorage.getItem("ip_address")
+    }, [])
+
+  // useEffect(() => {
+  //   // Проверяем, по какой ссылке перешел пользователь
+  //   const url = new URL(window.location.href)
+  //   IDfromURL.current = url.href.slice(33)
+
+  //   if (IDfromURL.current !== "" && IDfromURL.current !== 'null') {
+
+  //       if (ipAddress.current in user_dictionary) {
+
+  //           const user_id = user_dictionary[ipAddress.current]
+
+  //           const response = fetch('http://localhost:4000/session/get', { // Корректируем в соответствии с бэком
+  //               method: 'POST',
+  //               body: JSON.stringify({user_id: user_id, // Отправляем ID пользователя
+  //                                     session_id: IDfromURL.current})
+  //           })
+  //           .then(response => response.json())
+  //           .then(data => {
+  //               const history = JSON.parse(data)
+
+  //               history.forEach((mes) => {
+  //                   const ClientMessage = {
+  //                       data: mes.request,
+  //                       member: me
+  //                   }
+
+  //                   setMessages([...messages, ClientMessage])
+
+  //                   const MAIAMessage = { // Получаем сообщение от GPT
+  //                       content: mes.response,
+  //                       member: 'MAIA'
+  //                   }
+
+  //                   setMessages([...messages, MAIAMessage])
+  //               })
+  //           })
+  //           .catch(error => console.log(error))
+
+  //       }
+  //       else {
+  //           fetch('http://localhost:4000/session/create', { // Корректируем в соответствии с бэком
+  //           method: 'POST',
+  //           body: JSON.stringify({ip: ipAddress.current}) // Отправляем IP пользователя
+  //           })
+  //           .then(response => response.json())
+  //           .then(data => {
+  //               localStorage.setItem("session_id", data.session_id)
+  //               localStorage.setItem("user_id", data.user_id)
+  //           })
+
+  //           addUser(ipAddress.current, localStorage.getItem("user_id"))
+
+  //           router.push('?session_id='+localStorage.getItem("session_id"))
+  //       }
+  //   }
+  //   else{
+  //     const response = fetch('http://localhost:4000/session/create', { // Корректируем в соответствии с бэком
+  //             method: 'POST',
+  //             body: JSON.stringify({ip: ipAddress.current}) // Отправляем IP пользователя
+  //         })
+  //         .then(response => response.json())
+  //         .then(data => {
+  //             localStorage.setItem("session_id", data.session_id)
+  //             localStorage.setItem("user_id", data.user_id)
+  //         })
+
+  //         addUser(ipAddress.current, localStorage.getItem("user_id"))
+
+  //         router.push('?session_id='+localStorage.getItem("session_id"))
+  //   }
+  // }, [])
+
+  function onSendMessage(message) {
+    const newMessage = {
+      data: message,
+      member: me
+    }
+    setMessages([...messages, newMessage])
+  }
+
+  return(
+    <div>
+      <Parametrs UserID={user_dictionary[ipAddress.current]}/>
+      <div className={styles.appContent}>
+        <Messages messages={messages} me={me}/>
+        <Input onSendMessage={onSendMessage}/>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   )
 }
